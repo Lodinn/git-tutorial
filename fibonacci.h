ae23b59ca3a238d11e08f30309097b0e486b60ff
Lodinn <lodinn@lodinn.com>
2021-03-26 03:02:59 +0300

    space-optimized approach (stored depth = 2)

--- fibonacci.h
+++ fibonacci.h
@@ -1,7 +1,13 @@
 int fib(int n) {
-  if (n <= 1)
-    return n;
-  return fib(n-1) + fib(n-2);
+  int a = 0, b = 1, c, i;
+  if( n == 0)
+    return a;
+  for(i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 }
 
 
