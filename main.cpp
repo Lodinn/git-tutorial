295e70d44ed312e2829926e705aa4dd129f36577
Lodinn <lodinn@lodinn.com>
2021-03-26 03:09:57 +0300

    accepting user input; fib(46)~=INT_MAX

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "fibonacci.h"
 #include <cstdio>
+#include <stdlib.h>
+
+int get_int() {
+  int n = 0;
+  char buffer[128];
+  std::printf("Enter a number:\n");
+  fgets(buffer,sizeof(buffer),stdin);
+  n = atoi(buffer);
+  return ( n > 42 || n < 1 ) ? 0 : n;
+}
 
 int main () {
-  int n = 9;
-  std::printf("%d\n", fib(n));
+  int n = get_int();
+  std::printf("fib(%d) = %d\n", n, fib(n));
   return 0;
 }
